2d65bcedac01a52e962b017fc8976b4e09581d01
datatypevoid <david.r.niciforovic@gmail.com>
2016-02-28 12:30:21 -0500

    chore(polyfills): fix import

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,35 +2,33 @@
 import 'es6-shim';
 // (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
 import 'es6-promise';
-import 'zone.js/lib/browser/zone-microtask';
 import 'reflect-metadata';
 
 if ('production' === process.env.ENV) {
-  // Production
+    // Production
 
-  // In production Reflect with es7-reflect-metadata/reflect-metadata
-  // is added
+    // In production Reflect with es7-reflect-metadata/reflect-metadata is added
 
-  // Zone.js
-  require('zone.js/dist/zone-microtask.min');
+    // Zone.js
+    require('zone.js/dist/zone-microtask.min');
 
-  // RxJS
-  // In development manually include the operators you use
-  require('rxjs/add/operator/map');
-  require('rxjs/add/operator/mergeMap');
+    // RxJS
+    // In production manually include the operators you use
+    require('rxjs/add/operator/map');
+    require('rxjs/add/operator/mergeMap');
 
 } else {
-  // Development
+    // Development
 
-  Error['stackTraceLimit'] = Infinity;
+    Error['stackTraceLimit'] = Infinity;
 
-  require('zone.js/dist/zone-microtask');
-  require('zone.js/dist/long-stack-trace-zone');
+    require('zone.js/dist/zone-microtask');
+    require('zone.js/dist/long-stack-trace-zone');
 
-  // RxJS
-  // In development we are including every operator
-  require('rxjs/add/operator/map');
-  require('rxjs/add/operator/mergeMap');
+    // RxJS
+    // In development we are including every operator
+    require('rxjs/add/operator/map');
+    require('rxjs/add/operator/mergeMap');
 }
 
 // For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app

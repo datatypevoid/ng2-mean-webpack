caaef6ebdb67d6ccb8ab6a25fc5bfd23524ae8a8
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-02 22:57:12 -0500

    chore(recipes.reducer): create interface/reducer

---
+++ src/app/components/recipes/recipes.reducer.ts
@@ -0,0 +1,77 @@
+// ```
+// recipes.reducer.js
+// (c) 2016 David Newman
+// blackshuriken@hotmail.com
+// recipes.reducer.js may be freely distributed under the MIT license
+// ```
+
+// # Redux interface/reducer for `recipes`
+
+export interface Recipe {
+  _id: number;
+  tags: string[];
+  description: string;
+  ratings: number;
+  creator: string;
+  ingredients: string[];
+  directions: string[];
+};
+
+// A traditional `reducer` is a function which takes a `state`
+// object and an action to perform.
+
+// `ngrx` reducers work differently:
+//   * the second parameter is an object with the type of
+//     action to perform and the payload for that action
+
+// The `recipes` reducer performs actions on our list of `recipes`
+// Notice that we set `state` to a default value to initialize
+// smoothly
+export const recipes = (state: any = [], {type, payload}) => {
+
+  console.log('Recipes reducer hit! type: ');
+  console.log(type);
+  console.log('payload: ');
+  console.log(payload);
+  console.log('state: ');
+  console.log(state);
+
+  switch (type) {
+
+    // `ADD_RECIPES` returns whatever collection passed in as a
+    // new array
+    case 'ADD_RECIPES':
+      return payload;
+
+    // `CREATE_RECIPE` returns a new array by concatenating the
+    // existing recipe array with our new recipe
+    case 'CREATE_RECIPE':
+      return [...state, payload];
+
+    // `UPDATE_RECIPE` returns a new array by mapping to the current
+    // array, locating the recipe to update and cloning to create
+    // a new object using `Object.assign`
+    case 'UPDATE_RECIPE':
+      return state.map(recipe => {
+
+        console.log(recipe);
+
+        return recipe._id == payload._id
+          ? Object.assign({}, recipe, payload) : recipe
+      })
+
+    // `DELETE_RECIPE` returns a new array by filtering out the
+    // `recipe` that we want to delete
+    case 'DELETE_RECIPE':
+
+      return state.filter(recipe => {
+
+        console.log(recipe);
+
+        return recipe._id !== payload._id;
+      })
+
+    default:
+      return state;
+  }
+};

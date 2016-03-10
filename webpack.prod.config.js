e4cb0eec1a4d2ff6eb392ce3e84593b36aeea4f9
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-09 21:09:40 -0500

    chore(webpack.prod): include more classes to mangle except

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -174,7 +174,34 @@ module.exports = {
       // mangle: { screw_ie8 : true },//prod
       mangle: {
         screw_ie8 : true,
-        except: ['RouterLink', 'NgFor', 'NgModel'] // needed for uglify RouterLink problem
+        except: [
+          'RouterActive',
+          'RouterLink',
+          'RouterOutlet',
+          'NgFor',
+          'NgIf',
+          'NgClass',
+          'NgSwitch',
+          'NgStyle',
+          'NgSwitchDefault',
+          'NgModel',
+          'NgControl',
+          'NgFormControl',
+          'NgForm',
+          'AsyncPipe',
+          'DatePipe',
+          'JsonPipe',
+          'NumberPipe',
+          'DecimalPipe',
+          'PercentPipe',
+          'CurrencyPipe',
+          'LowerCasePipe',
+          'UpperCasePipe',
+          'SlicePipe',
+          'ReplacePipe',
+          'I18nPluralPipe',
+          'I18nSelectPipe'
+        ] // needed for uglify RouterLink problem
       },// prod
       compress : { screw_ie8 : true },//prod
       comments: false//prod
